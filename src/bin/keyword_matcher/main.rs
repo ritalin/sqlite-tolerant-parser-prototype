6677dd67ff7edf37cf61e4e0ad5e390f776c2d3a
Ritalin <kazu.ktz.tamura@gmail.com>
2025-04-10 13:58:12 +0900

    キーワード抽出の実装

---
+++ src/bin/keyword_matcher/main.rs
@@ -0,0 +1,31 @@
+use std::ffi::CString;
+use sqlite_parser_proto::binding::lemon::{self, SymbolType};
+
+pub fn main() {
+    let filename = CString::new("submodules/sqlite/src/parse.y").unwrap();
+
+    let mut lem = lemon::LemonBuilder::new()
+        .set_args(std::env::args())
+        .set_grammar(&filename)
+        .build()
+    ;
+    
+    lem.parse();
+
+    let prefix = lem.symbol_prefix();
+
+    let iter = lem.symbols()
+        .skip(1)
+        .filter(|x| match x.symbol_type() {
+            SymbolType::Terminal{..} => true,
+            _ => false,
+        }).enumerate()
+    ;
+
+    for (i, symbol) in iter {
+        let name = symbol.name();
+        if name != "{default}" {
+            println!("#define {}{} {}", prefix, symbol.name(), i +1);
+        }
+    }
+}
